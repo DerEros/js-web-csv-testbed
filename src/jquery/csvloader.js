700bcb2051a83026c1c6431eb04ad0d1de9747be
DerEros <DerEros@users.noreply.github.com>
2018-03-01 09:04:09 +0100

    Accessing result object

--- src/jquery/csvloader.js
+++ src/jquery/csvloader.js
@@ -1,4 +1,5 @@
 var $ = require("jquery");
+var papa = require("papaparse");
 
 function app() {
     $.get("/data.csv")
@@ -7,11 +8,23 @@ function app() {
 }
 
 function handleDataReceived(data) {
-    alert("Got data! Data: " + data);
+    var result = papa.parse(data, {
+        header: true
+    });
+    var parsedData = result.data;
+    var fields = result.meta.fields;
+
+    renderData(fields, parsedData);
 }
 
 function handleDataFailed(status) {
-    alert("Getting data failed! " + status);
+    alert("Getting data failed with code " + status.status + " (" + status.statusText + ")");
+}
+
+function renderData(fields, data) {
+    $.each(data, function(idx, row) {
+        console.log("Row:", row);
+    });
 }
 
 $(document).ready(function() {
