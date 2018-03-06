59903601213cd48688f7f275beee52018b314933
Eros Candelaresi <eros@candelaresi.de>
2018-03-06 21:54:58 +0100

    Added sorting with both directions

--- src/jquery/datamodel.js
+++ src/jquery/datamodel.js
@@ -2,17 +2,47 @@ var _ = require("lodash");
 
 export default {
     create: function(fieldNames, items) {
+        var SortDirection = Object.freeze({"ASCENDING": 1, "DESCENDING": 2});
+
+        function toggleDirection(previousDirection) {
+            if (previousDirection == SortDirection.ASCENDING) {
+                return SortDirection.DESCENDING;
+            } else {
+                return SortDirection.ASCENDING;
+            }
+        }
+
+        function sortItems(items, sortingModel) {
+            if (sortingModel.direction == SortDirection.ASCENDING) {
+                console.log("Sorting ascending");
+                return _.sortBy(items, [sortingModel.fieldName]);
+            } else {
+                console.log("Sorting descending");
+                return _.sortBy(items, [sortingModel.fieldName]).reverse();
+            }
+        }
+        
         return {
-            sortByField: fieldNames[0],
+            sortByField: {
+                fieldName: fieldNames[0],
+                direction: SortDirection.ASCENDING
+            },
 
             setSortBy: function(fieldName) {
-                this.sortByField = fieldName;
+                if (fieldName == this.sortByField.fieldName) {
+                    this.sortByField.direction = toggleDirection(this.sortByField.direction);
+                } else {
+                    this.sortByField = { 
+                        fieldName: fieldName,
+                        direction: SortDirection.ASCENDING
+                    }
+                }
             },
 
             getFieldNames: function() { return fieldNames },
 
             getItems: function() { 
-                return _.sortBy(items, [this.sortByField]);
+                return sortItems(items, this.sortByField);
             }
         }
     },
